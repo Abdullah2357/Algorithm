06a704be031270308fa58545628b17d6d0b951b7
Md. Abdullah <75635181+Abdullah2357@users.noreply.github.com>
2024-10-05 14:01:42 +0600

    Add files via upload

---
+++ Geometry/CF 600D - Area of Two Circles' Intersection.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define sz      200005
+const double eps = 1e-9;
+const double PI = acos((double)-1.0);
+
+typedef double T;
+#define double long double
+int sign(double x) { return (x > eps) - (x < -eps); }
+struct PT {
+    double x, y;
+    PT() { x = 0, y = 0; }
+    PT(double x, double y) : x(x), y(y) {}
+    PT(const PT &p) : x(p.x), y(p.y)    {}
+    PT operator + (const PT &a) const { return PT(x + a.x, y + a.y); }
+    PT operator - (const PT &a) const { return PT(x - a.x, y - a.y); }
+    PT operator * (const double a) const { return PT(x * a, y * a); }
+    friend PT operator * (const double &a, const PT &b) { return PT(a * b.x, a * b.y); }
+    PT operator / (const double a) const { return PT(x / a, y / a); }
+    bool operator == (PT a) const { return sign(a.x - x) == 0 && sign(a.y - y) == 0; }
+    bool operator != (PT a) const { return !(*this == a); }
+    bool operator < (PT a) const { return sign(a.x - x) == 0 ? y < a.y : x < a.x; }
+    bool operator > (PT a) const { return sign(a.x - x) == 0 ? y > a.y : x > a.x; }
+    double norm() { return sqrt(x * x + y * y); }
+    double norm2() { return x * x + y * y; }
+    PT perp() { return PT(-y, x); }
+    double arg() { return atan2(y, x); }
+    PT truncate(double r) { // returns a vector with norm r and having same direction
+        double k = norm();
+        if (!sign(k)) return *this;
+        r /= k;
+        return PT(x * r, y * r);
+    }
+};
+
+
+double circle_circle_area(PT a, double r1, PT b, double r2) {
+    double d = (a - b).norm();
+    if(r1 + r2 < d + eps) return 0;
+    if(r1 + d < r2 + eps) return PI * r1 * r1;
+    if(r2 + d < r1 + eps) return PI * r2 * r2;
+    double theta_1 = acos((r1 * r1 + d * d - r2 * r2) / (2 * r1 * d)), 
+        theta_2 = acos((r2 * r2 + d * d - r1 * r1)/(2 * r2 * d));
+    return r1 * r1 * (theta_1 - sin(2 * theta_1)/2.) + r2 * r2 * (theta_2 - sin(2 * theta_2)/2.);
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    double x1, y1, r1, x2, y2, r2;
+    cin>>x1>>y1>>r1>>x2>>y2>>r2;
+    cout<<fixed<<setprecision(8)<< circle_circle_area({(T)x1, (T)y1}, (T)r1, {(T)x2, (T)y2}, (T)r2)<<endl;
+    return 0;
+}
